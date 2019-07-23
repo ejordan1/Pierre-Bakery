6a00bcf0b160282908180e762ecdcf2dd0614284
Student <example@example.com>
2019-07-23 12:09:14 -0700

    remove object properties to make work again

--- Bread.cs
+++ Bread.cs
@@ -1,15 +1,12 @@
-namespace Models{
-    public class Bread : Food{
-        private string _breadType {get; set;}
-        private bool _halfLoaf {get; set;}
-        public Bread(Date madeDate, Date expDate, string breadType){
-            this._madeDate = date;
-            this._breadType = breadType;
-            this._expDate = expDate;
-            this._halfLoaf = false;
-        }
+namespace Models
+{
+    public class Bread
+    {
+        private string _breadType { get; set; }
+        private bool _halfLoaf { get; set; }
 
-        public static int BreadTotalPrice(int numberOfBreads){
+        public static int BreadTotalPrice(int numberOfBreads)
+        {
             int n = numberOfBreads / 3;
             return n * 10 + (numberOfBreads % 3) * 5;
         }
--- Pastry.cs
+++ Pastry.cs
@@ -1,14 +1,10 @@
-namespace Models{
-    public class Pastry{
-        public string _pastryType {get;}
-        
-        public Pastry(Date madeDate, Date expDate, string pastryType){
-            this._madeDate = date;
-            this._expDate = expDate;
-            this._pastryType = pastryType;
-        }
+namespace Models
+{
+    public class Pastry
+    {
 
-        public static int PastriesTotalPrice(int numberOfPastries){
+        public static int PastriesTotalPrice(int numberOfPastries)
+        {
             int n = numberOfPastries / 3;
             return n * 5 + (numberOfPastries % 3) * 2;
         }
